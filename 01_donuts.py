4992134d1cf7c6531f096991abe0cd064ed36e84
Anderson de Araújo Alves <anderson.araujoa@gmail.com>
2020-10-24 21:31:53 -0400

    resolução do desafio 01_donuts.py

--- 01_donuts.py
+++ 01_donuts.py
@@ -9,12 +9,17 @@ Exemplo: donuts(5) retorna 'Number of donuts: 5'
 e donuts(23) retorna 'Number of donuts: many'
 """
 
+
 def donuts(count):
     # +++ SUA SOLUÇÃO +++
-    return
-
+    if count >= 10:
+        count = 'many'
+        return f'Number of donuts: {count}'
+    else:
+        return f'Number of donuts: {count}'
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
+
 
 def test(f, in_, expected):
     """
