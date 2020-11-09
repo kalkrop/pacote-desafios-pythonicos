117d54b2d782c958b83516c41b8fa1ee079d3308
Anderson de Araújo Alves <anderson.araujoa@gmail.com>
2020-11-08 21:59:15 -0400

    resolução do desafio 06_not_bad.py

--- 06_not_bad.py
+++ 06_not_bad.py
@@ -8,13 +8,24 @@ por 'good' e retorne a string resultante.
 
 Exemplo: 'The dinner is not that bad!' retorna 'The dinner is good!'
 """
+import re
+
 
 def not_bad(s):
     # +++ SUA SOLUÇÃO +++
-    return
+    '''
+    temporario = s
+    bad_index, not_index = s.find('bad'), s.find('not')
+    if not_index < bad_index:
+        temporario = s.replace(s[not_index:(bad_index+3)], "good")
 
+    return temporario
+    '''
 
-# --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
+    return re.sub(r'(not).*?(bad)', r'good', s)
+
+    # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
+
 
 def test(f, in_, expected):
     """
@@ -39,3 +50,7 @@ if __name__ == '__main__':
     test(not_bad, 'This dinner is not that bad!', 'This dinner is good!')
     test(not_bad, 'This tea is not hot', 'This tea is not hot')
     test(not_bad, "It's bad yet not", "It's bad yet not")
+    test(not_bad, "Bad but not so bad", "Bad but good")
+    test(not_bad, "E se tiver not bad e depois um not bad novamente",
+         "E se tiver good e depois um good novamente")
+    test(not_bad, "E se tiver not not bad", "E se tiver good")
